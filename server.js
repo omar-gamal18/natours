8b9f353bf94c1bb2753bd2cec66326fe599de41c
Omar Gamal <omar.gamal18200588@gmail.com>
2026-03-25 23:56:53 +0200

    add new files

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const tourRoutes = require('./routes/tourRoutes');
 
-const port = 5000;
+dotenv.config({ path: './config.env' });
 const app = express();
-
 app.use(express.json());
 
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
 app.use('/api/v1/tours', tourRoutes);
 
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
+const port = process.env.PORT;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connected successfully');
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('DB connection failed:', err);
+  });
