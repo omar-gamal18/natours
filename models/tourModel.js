5b9ee4e40918ce53f21c56c01f11297a72a64bc2
Omar Gamal <omar.gamal18200588@gmail.com>
2026-03-26 04:55:13 +0200

    Update schema  and remove old files

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: [true, 'the name is required'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'the durations is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'the group size is required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'the difficulty is required'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'the price is required'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'the description is required'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'the image is required'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
