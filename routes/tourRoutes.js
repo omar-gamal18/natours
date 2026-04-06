3c78303e0c028f0876640ddad7a593b362253801
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-06 16:02:45 +0200

    add aggregation(match,group,sort

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,7 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 
 router.route('/top-5').get(tourController.getTop5, tourController.getAllTours);
+router.route('/agg').get(tourController.getToursStatus);
 
 router
   .route('/')
