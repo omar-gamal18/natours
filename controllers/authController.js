efe84a6652a9af3f01df082ee88f44f6de8de164
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-16 02:36:05 +0200

    fix bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,15 @@
 const User = require('../models/userModel');
 
 exports.signUp = async (req, res, next) => {
-  const user = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
-    user,
+    user: newUser,
   });
 };
