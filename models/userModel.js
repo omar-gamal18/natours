8f16bde808c1fabaf4896eb70a9c9a1a489c3ec6
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-20 00:36:54 +0200

    "feat(auth): generate password reset token"

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -37,6 +39,8 @@ const userSchema = mongoose.Schema(
       },
     },
     passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
   {
     timestamps: true,
@@ -66,6 +70,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   console.log(JWTTimestamp, '    ', changedTimestamp);
 
   return JWTTimestamp < changedTimestamp;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetExpires = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
