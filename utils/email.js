9f37a3a4beb014d40398b8a7c122df799f8d912f
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-20 05:49:29 +0200

    fix(auth): send emails

--- utils/email.js
+++ utils/email.js
@@ -1,40 +1,14 @@
-const nodemailer = require('nodemailer');
-
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      //   pass: process.env.EMAIL_PASSWORD,
-      pass: '0f2256735f3621',
-    },
-    logger: true,
-    debug: true,
-  });
-
-  const mailOptions = {
-    from: 'omar gamal <omar.gamal18200588@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-
-  await transporter.sendMail(mailOptions);
-};
-
-module.exports = sendEmail;
-
 // const nodemailer = require('nodemailer');
 
 // const sendEmail = async (options) => {
 //   const transporter = nodemailer.createTransport({
-//     host: 'sandbox.smtp.mailtrap.io',
-//     port: 2525,
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT),
+//     secure: false,
 //     auth: {
-//       user: '676aed71c5997c',
-//       pass: '****3621',
+//       user: process.env.EMAIL_USERNAME,
+//       //   pass: process.env.EMAIL_PASSWORD,
+//       pass: '0f2256735f3621',
 //     },
 //     logger: true,
 //     debug: true,
@@ -51,3 +25,26 @@ module.exports = sendEmail;
 // };
 
 // module.exports = sendEmail;
+
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'omar gamal <omar.gamal18200588@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
