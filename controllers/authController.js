d765f055a812a178d9c6dd964e73f22c55d3dd09
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-20 23:21:08 +0200

    feat(auth): verify password reset token update user password after reset and clear token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
+
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
@@ -84,7 +86,7 @@ exports.protect = async (req, res, next) => {
   }
 
   // 6. GRANT ACCESS TO PROTECTED ROUTES
-  console.log(req.user);
+  req.user = cuurentUser;
   next();
 };
 
@@ -132,4 +134,33 @@ exports.forgotPassword = async (req, res, next) => {
       ),
     );
   }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.confirmPassword = req.body.confirmPassword;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 };
