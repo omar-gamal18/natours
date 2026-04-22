3fc95a426dfc29ce4b01618b488a66e32703721e
Omar Gamal <omar.gamal18200588@gmail.com>
2026-04-22 03:24:24 +0200

    Data Sanitization and Preventing Parameter Pollution(end of the section(auth))

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const ErrorHandling = require('./controllers/errorController');
@@ -26,6 +29,26 @@ app.use('/api', limiter);
 
 // body parser
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// HTTP Parameter Pollution Prevention
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 process.on('uncaughtException', (err) => {
   console.error('UNHANDLED Exception 💥', err);
