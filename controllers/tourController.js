099ca72a427f485921687785c0b8997468b1bb3d
Omar Gamal <omar.gamal18200588@gmail.com>
2026-06-19 20:53:23 +0300

    fix(Processing Multiple Images)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,7 +32,33 @@ exports.uploadTourImages = upload.fields([
   { name: 'images', maxCount: 3 },
 ]);
 
-exports.resizeTourImages = (req, res, next) => {
+exports.resizeTourImages = async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  const imageCoverFilename = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`puplic/img/tours/${imageCoverFilename}`);
+  req.body.imageCover = imageCoverFilename;
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`puplic/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    }),
+  );
+
   next();
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -21,16 +21,19 @@ const upload = mutter({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
-exports.resizeUserPhoto = (req, res, next) => {
+
+exports.resizeUserPhoto = async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`puplic/img/users/${req.file.filename}`);
+
+  next();
 };
 
 exports.getAllUsers = factory.getAll(User);
